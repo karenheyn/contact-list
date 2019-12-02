95294963e7de874fafe50174c82fe9763a561ad7
karenheyn <karenheyn@gmail.com>
2019-12-02 14:22:16 -0500

    done

--- main.py
+++ main.py
@@ -21,7 +21,41 @@ class Contact(Basemodel):
 db.connect()
 db.create_tables([Contact])
 
-contact_prompt = input("would you like to create a new contact y/n")
-if contact_prompt == "y":
-    name_prompt = input("name")
-    name
+
+def create_contact():
+    contact_prompt = str(input("would you like to create a new contact y/n "))
+    if contact_prompt == "y":
+        name_prompt = str(input("name "))
+        year = int(input("what year were they born? "))
+        month = int(input("what month were they born? "))
+        day = int(input("what day were they born? "))
+        number_prompt = str(input("phone number "))
+        new_contact = Contact(name=name_prompt, birthday=date(
+            year, month, day), number=number_prompt)
+        new_contact.save()
+        create_contact()
+    list_prompt = str(input("would you like to view all contacts y/n "))
+    if list_prompt == "y":
+        list_all()
+    search_prompt = str(
+        input("would you like search contacts y/n "))
+    if search_prompt == "y":
+        search()
+    else:
+        sys.exit()
+
+
+def list_all():
+    list_prompt = input("would you like to list all contacts y/n ")
+    if list_prompt == "y":
+        for contact in Contact.select():
+            print(f"{contact.name}\n{contact.birthday}\n{contact.number}\n\n")
+
+
+def search():
+    search_prompt = str(input("who would you like to search for? "))
+    searched = Contact.select().where(Contact.name.contains(search_prompt)).get()
+    print(f"{searched.name}\n{searched.birthday}\n{searched.number}")
+
+
+create_contact()
